135e7bbcdbc965242b0ad235969356ce78d9cdfc
Oleksiy Kovyrin <oleksiy.kovyrin@shopify.com>
2022-12-16 10:04:00 -0500

    Redirect all requests to the index page by default

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import React, {useState, useEffect} from 'react';
 
 import Layout from "./pages/Layout";
@@ -43,6 +43,7 @@ function App() {
           } />
           <Route path="*" element={<NoPage />} />
         </Route>
+        <Route path="*" element={<Navigate to="/words-of-the-week" />} />
       </Routes>
     </BrowserRouter>
   );

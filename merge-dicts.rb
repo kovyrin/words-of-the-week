0bd73b8bcd5127a7cdab45ed3744b7ac70fe39cb
Oleksiy Kovyrin <oleksiy.kovyrin@shopify.com>
2022-12-17 20:13:26 -0500

    Filter out bad English words

--- merge-dicts.rb
+++ merge-dicts.rb
@@ -16,6 +16,16 @@ class MergedDict
     dict[word] += Array(translations)
   end
 
+  def remove_words(words)
+    words.each do |word|
+      dict.delete(word)
+    end
+
+    dict.each do |word, translations|
+      translations.reject! { |t| words.include?(t) }
+    end
+  end
+
   def compact
     compact_dict = {}
     dict.each do |word, translations|
@@ -32,12 +42,16 @@ apertium_dict_file = 'dicts/apertium-fra-eng.fra-eng.json'
 dictcc_dict_file = 'dicts/dict.cc.fr-en.json'
 top1000_dict_file = 'dicts/top-1000.csv'
 top5000_dict_file = 'dicts/5000_wordlist_french.csv'
+bad_words_file = 'dicts/bad-words.txt'
 
 # Merge both dicts, keeping only the 5 shortest translations for each word
 apertium_dict = JSON.parse(File.read(apertium_dict_file))
 dictcc_dict = JSON.parse(File.read(dictcc_dict_file))
 top1000_dict = CSV.read(top1000_dict_file, col_sep: ',', headers: true)
 top5000_dict = CSV.read(top5000_dict_file, col_sep: ',', headers: true)
+
+# Load the list of known bad words
+bad_words = File.readlines(bad_words_file).map(&:downcase).map(&:strip)
 
 dict = MergedDict.new
 dictcc_dict.each do |word, translations|
@@ -52,12 +66,22 @@ top1000_dict.each do |row|
   dict.add(row['French'], row['English'])
 end
 
+top_words = []
 top5000_dict.each do |row|
   translations = row['word_en'].split(',').map(&:strip)
   dict.add(row['word_fr'], translations)
+  top_words << row['word_fr']
 end
+
+# Remove bad words
+dict.remove_words(bad_words)
 
 # Write the merged dict to a file
 File.open('src/fra-eng.json', 'w') do |f|
   f.write(JSON.pretty_generate(dict.compact))
+end
+
+# Write the top 5000 words to a file
+File.open('src/top-5000.json', 'w') do |f|
+  f.write(JSON.pretty_generate(top_words))
 end
